32b1aa8e617dd4e7005969df4ed052105cd4ab13
Serzsh <serzants.janis@gmail.com>
2023-02-06 14:18:23 +0200

    removed cors

--- src/server.js
+++ src/server.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-
+const cors = require('cors')
 const sqlite3 = require('sqlite3').verbose();
 const database = new sqlite3.Database("./src/db/database.db");
 
 const app = express()
 const port = 3004
+
+app.use(cors({
+  origin: '*'
+}))
 
 app.use(bodyParser.json());
 
@@ -19,12 +23,22 @@ database.serialize(() => {
 
 });
 
+// controlieris kurš atbild par to, kad tiks prasīts GET piepeasījums uz root, 
+// jeb šajā gadījumā http://localhost:3004/
 app.get('/', (req, res) => {
 
   database.get(`SELECT * FROM movies`,(err, row) => {
     res.json(row);
   })
 })
+
+// controlieris kurš atbild par to, kad tiks prasīts GET piepeasījums uz root, 
+// jeb šajā gadījumā http://localhost:3004/
+app.get('/hello-world', (req, res) => {
+
+  res.json({message: "hello back"});
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
